df7c09aba4e7dce92461766d9727d9b1bd81386f
NicoleLozadaLeon <lozadaleonn@gmail.com>
2024-04-23 19:06:10 -0400

    añadí las fórmulas para MRUA

--- cod.fuente/Program.cs
+++ cod.fuente/Program.cs
@@ -2,33 +2,33 @@
 Console.WriteLine("*****CALCULADORA DE MRU Y MRUA*****");
 Console.WriteLine("*Calculadora de MRU = 1");
 Console.WriteLine("*Calculadora de MRUA = 2");
-Console.WriteLine("Seleccione una opción (1 -2)");
+Console.WriteLine("Seleccione una opción (1 - 2)");
 int option = Convert.ToInt32(Console.ReadLine());
 while( option == 1){
     Console.WriteLine("¿Que desea calcular?");
-    Console.WriteLine("* Distancia = 3    * Velocidad = 4     * Tiempo = 5");
+    Console.WriteLine("* Distancia = 1    * Velocidad = 2     * Tiempo = 3");
     int calculo1 = Convert.ToInt32(Console.ReadLine());
     switch (calculo1){
-        case 3:
+        case 1:
             Console.WriteLine("Ingrese el valor de la velocidad");
             int v3 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Ingrese el valor del tiempo");
             int t3 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("{0}", v3 * t3);
+            Console.WriteLine("La distancia es {0}", v3 * t3);
             break;
-        case 4:
+        case 2:
             Console.WriteLine("Ingrese el valor de la distancia");
             int d4 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Ingrese el valor del tiempo");
             int t4 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("{0}", d4 / t4);
+            Console.WriteLine("La velocidad es {0}", d4 / t4);
             break;
-        case 5:
+        case 3:
             Console.WriteLine("Ingrese el valor de la distancia");
             int d5 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Ingrese el valor de la velocidad");
             int v5 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("{0}", d5 / v5);
+            Console.WriteLine("El tiempo es {0}", d5 / v5);
             break;
     }
 
@@ -36,10 +36,37 @@ while( option == 1){
 }
 while( option == 2){
     Console.WriteLine("¿Que desea calcular?");
-    Console.WriteLine("* Posición final = 6    *  Velocidad final = 7     * Tiempo = 8");
+    Console.WriteLine("* Posición final = 1    *  Velocidad final = 2     * Tiempo = 3");
     int calculo2 = Convert.ToInt32(Console.ReadLine());
     switch (calculo2){
-        case 6:
+        case 1:
+            Console.WriteLine("Ingrese el valor de la posción inicial");
+            int x01 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de la velocidad inicial");
+            int v01 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del tiempo");
+            int t1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de la aceleración");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("La posición final es {0}", x01 + v01 * t1 + 0.5 * a1 * (t1 * t1));
+            break;
+        case 2:
+            Console.WriteLine("Ingrese el valor de la velocidad inicial");
+            int v02 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del tiempo");
+            int t2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de la aceleración");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("La velocidad final es {0}", v02 + a2 * t2 );
+            break;
+        case 3:
+            Console.WriteLine("Ingrese el valor de la velocidad inicial");
+            int v03 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de la velocidad final");
+            int v3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de la aceleración");
+            int a3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("La velocidad final es {0}", (v3 - v03) / a3 );
             break;
     }
     
--- cod.fuente/obj/Debug/net8.0/calculadora.AssemblyInfo.cs
+++ cod.fuente/obj/Debug/net8.0/calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e975919565e2547175e3bca34c77ac49e1829ec8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c11f6b42f2dfea6dae32b786324b8a259a6ffdb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
