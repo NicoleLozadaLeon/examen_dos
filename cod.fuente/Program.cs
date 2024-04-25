1aa8523fe73ec6bbbb7ea7c57393218a35655d41
NicoleLozadaLeon <lozadaleonn@gmail.com>
2024-04-25 15:55:19 -0400

    corrigiendo error de F1 MRUA

--- cod.fuente/Program.cs
+++ cod.fuente/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("*****CALCULADORA DE MRU Y MRUA*****"); //diseño del programa
 Console.WriteLine("*Calculadora de MRU = 1");
@@ -13,27 +13,27 @@ while(option == 1){   //MRU: opcion ==1
         switch (calculo1){
             case 1:
                 Console.WriteLine("***** BIENVENIDO A LA CALCULADORA DE MRU *****");      
-                Console.WriteLine("Ingrese el valor de la velocidad");      
+                Console.WriteLine("Ingrese el valor de la velocidad km/h");      
                 double v3 = Convert.ToDouble(Console.ReadLine());             //Ingresar un número para cada variable
-                Console.WriteLine("Ingrese el valor del tiempo");
+                Console.WriteLine("Ingrese el valor del tiempo en h");
                 double t3 = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("La distancia es {0}", v3 * t3);        //lógica usando la formula de la distancia
+                Console.WriteLine("La distancia es {0} en km ", v3 * t3);        //lógica usando la formula de la distancia
                 break;
             case 2:
                 Console.WriteLine("***** BIENVENIDO A LA CALCULADORA DE MRU *****");
-                Console.WriteLine("Ingrese el valor de la distancia");
+                Console.WriteLine("Ingrese el valor de la distancia en km");
                 double d4 = Convert.ToDouble(Console.ReadLine());             //Ingresar un número para cada variable
-                Console.WriteLine("Ingrese el valor del tiempo");
+                Console.WriteLine("Ingrese el valor del tiempo en horas");
                 double t4 = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("La velocidad es {0}", d4 / t4);          //lógica usando la formula de la velocidad
+                Console.WriteLine("La velocidad es {0} km/h", d4 / t4);          //lógica usando la formula de la velocidad
                 break;
             case 3:
                 Console.WriteLine("***** BIENVENIDO A LA CALCULADORA DE MRU *****");
-                Console.WriteLine("Ingrese el valor de la distancia");          //Ingresar un número para cada variable
+                Console.WriteLine("Ingrese el valor de la distancia en km ");          //Ingresar un número para cada variable
                 double d5 = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Ingrese el valor de la velocidad");
+                Console.WriteLine("Ingrese el valor de la velocidad km/h");
                 double v5 = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("El tiempo es {0}", d5 / v5);             //lógica usando la formula del tiempo
+                Console.WriteLine("El tiempo es {0} horas", d5 / v5);             //lógica usando la formula del tiempo
                 break;
         }
         Console.WriteLine("Desea hacer otro cálculo?:");
@@ -60,35 +60,36 @@ while(option == 2){     //MRUA: opcion == 2
     switch (calculo2){
         case 1:
             Console.WriteLine("***** BIENVENIDO A LA CALCULADORA DE MRUA *****");
-            Console.WriteLine("Ingrese el valor de la posción inicial");
+            Console.WriteLine("Ingrese el valor de la posción inicial en metros");
             double x01 = Convert.ToDouble(Console.ReadLine());                       //Ingresar un número para cada variable
-            Console.WriteLine("Ingrese el valor de la velocidad inicial");
+            Console.WriteLine("Ingrese el valor de la velocidad inicial en km/h");
             double v01 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Ingrese el valor del tiempo");
+            Console.WriteLine("Ingrese el valor del tiempo en horas");
             double t1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Ingrese el valor de la aceleración");
+            Console.WriteLine("Ingrese el valor de la aceleración en m/s2");
             double a1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("La posición final es {0}", x01 + v01 * t1 + 0.5 * a1 * (t1 * t1));     //lógica usando la formula de la posición final
+            double ans = x01 + v01 * t1 + 0.5 * a1 * Math.Pow(t1, t1);
+            Console.WriteLine($"La posición final es {ans} km");     //lógica usando la formula de la posición final
             break;
         case 2:
             Console.WriteLine("***** BIENVENIDO A LA CALCULADORA DE MRUA *****");
-            Console.WriteLine("Ingrese el valor de la velocidad inicial");
+            Console.WriteLine("Ingrese el valor de la velocidad inicial en km/ h");
             double v02 = Convert.ToDouble(Console.ReadLine());                              //Ingresar un número para cada variable
-            Console.WriteLine("Ingrese el valor de la aceleración");
+            Console.WriteLine("Ingrese el valor de la aceleración en m/s2");
             double a2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Ingrese el valor del tiempo");
+            Console.WriteLine("Ingrese el valor del tiempo en horas");
             double t2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("La velocidad final es {0}", v02 + a2 * t2 );             //lógica usando la formula de la velocidad
+            Console.WriteLine("La velocidad final es {0} km/h", v02 + a2 * t2 );             //lógica usando la formula de la velocidad
             break;
         case 3:
             Console.WriteLine("***** BIENVENIDO A LA CALCULADORA DE MRUA *****");
-            Console.WriteLine("Ingrese el valor de la velocidad final");
+            Console.WriteLine("Ingrese el valor de la velocidad final km/h");
             double v3 = Convert.ToDouble(Console.ReadLine());                            //Ingresar un número para cada variable
-            Console.WriteLine("Ingrese el valor de la velocidad inicial");
+            Console.WriteLine("Ingrese el valor de la velocidad inicial en km/h");
             double v03 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Ingrese el valor de la aceleración");
+            Console.WriteLine("Ingrese el valor de la aceleración en m/s2");
             double a3 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("El tiempo es {0}", (v3 - v03) / a3 );                 //lógica usando la formula del tiempo
+            Console.WriteLine("El tiempo es {0} horas", (v3 - v03) / a3 );                 //lógica usando la formula del tiempo
             break; 
     }
     Console.WriteLine("Desea hacer otro cálculo?:");
--- cod.fuente/obj/Debug/net8.0/calculadora.AssemblyInfo.cs
+++ cod.fuente/obj/Debug/net8.0/calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca7c2c3adeb77d9029d3a70636b2011a47d7ca03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae43d5aeb2ea016d91f8b639f3093bf84f271069")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
